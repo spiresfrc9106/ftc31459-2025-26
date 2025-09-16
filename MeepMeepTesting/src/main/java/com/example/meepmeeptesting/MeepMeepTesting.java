56a3afbe6dd24f1589491f7742a25f8c9162e656
endert1099 <endert1099@gmail.com>
2025-09-16 11:59:59 +1000

    Added Decode field

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -6,26 +6,36 @@ import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
 import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep) // BotBuilder... say that again...
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(61, 0, Math.toRadians(180)))
+                        .forward(0.01)
+                        //.turn(Math.toRadians(90))
+                        //.forward(30)
+                        //.turn(Math.toRadians(90))
+                        //.forward(30)
+                        //.turn(Math.toRadians(90))
+                        //.forward(30)
+                        //.turn(Math.toRadians(90))
                         .build());
 
+        Image background = null;
+        try {
+            background = ImageIO.read(new File("MeepMeepTesting/src/main/java/com/example/meepmeeptesting/decode.png"));
+        } catch (IOException ignored) {}
+        assert background != null;
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
+        meepMeep.setBackground(background)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
