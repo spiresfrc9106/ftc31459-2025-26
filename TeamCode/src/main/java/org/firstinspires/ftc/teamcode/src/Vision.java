b69f354607d6c88bed320369a1acb698ae74ae34
David Nguyen <infernaldestinymc@gmail.com>
2025-09-18 18:17:25 -0700

    Created Vision.java and Apriltag detection

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/Vision.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.src;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+@TeleOp
+public class Vision extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .build();
+
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(1920, 1080))
+                .build();
+
+
+        while (!isStopRequested() && opModeIsActive()){
+            if (tagProcessor.getDetections().size() > 0){
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+            }
+        }
+    }
+
+
+
+
+
+
+}
